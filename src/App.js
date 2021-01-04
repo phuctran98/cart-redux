b05e3b45536337b80821679a029c508645d65f5f
phuctran98 <nguyentranphuc165@gmail.com>
2021-01-04 15:56:23 +0700

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './Components/Header'
+import Products from './Components/Products'
+import Message from './Components/Message'
+import Cart from './Components/Cart'
+import Footer from './Components/Footer'
+class App extends React.Component {
+    render() {
+        return (
+            <div className= "hidden-sn animated deep-purple-skin">
+                <Header></Header>
+                <Products></Products>
+                <Message></Message>
+                <Cart></Cart>
+                <Footer></Footer>
+            </div>  
+        );
+    }
 }
+
 
 export default App;
---
+++ src/Components/Cart.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import CartResult from './CartResult'
+import CartItem from './CartItem'
+
+class Cart extends React.Component {
+    render() {
+        return (
+            <section className="section">
+                <div className="table-responsive">
+                    <table className="table product-table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Sản Phẩm</th>
+                                <th>Giá</th>
+                                <th>Số Lượng</th>
+                                <th>Tổng Cộng</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <CartItem></CartItem>
+                            <CartResult></CartResult>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+
+        );
+    }
+}
+
+
+export default Cart;
